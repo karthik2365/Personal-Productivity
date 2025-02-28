49e30eedc9266832d03478ed396fc4f1bd7b9a78
karthik <24eg105p59@anurag.edu.in>
2025-02-28 20:05:50 +0530

    community forrum

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,14 +8,14 @@ import { silkScreen } from '@/app/layout'
 function Navbar() {
   return (
     <div className='h-16 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-green-500 flex justify-between items-center'>
-      <Link href="/" className={`${silkScreen.className} text-4xl font-bold text-black dark:text-white`}>
+      <Link href="/" className={`${silkScreen.className} text-4xl font-bold text-green-500`}>
         Prodo
       </Link>
 
       <div className="flex items-center gap-6">
         <ModeToggle />
 
-        <Link href="/dashboard" className="flex items-center gap-2 text-primary bg-secondary px-4 py-2 rounded-lg hover:bg-accent-dark transition">
+        <Link href="/dashboard" className="flex items-center gap-2 text-green-500 bg-secondary px-4 py-2 rounded-lg hover:bg-accent-dark transition">
           <LayoutDashboard size={20} />
           <span>Dashboard</span>
         </Link>
